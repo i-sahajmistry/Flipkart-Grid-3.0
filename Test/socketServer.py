232b8f010b24fbbbe42f0069905dc2a22df7d95b
Divya <sangwandivya2002@gmail.com>
2021-11-13 12:37:28 +0530

    bug fixis

--- Test/socketServer.py
+++ Test/socketServer.py
@@ -2,14 +2,14 @@ import socket
 import cv2 as cv
 import json
 
-ports = [1234, 1239, 1240, 1241]
+ports = [2222, 1239, 1240, 1241]
 s = []
 for i in range(1):
     s.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
     s[i].bind(('0.0.0.0', ports[i]))
     s[i].listen(0)
 
-y = {"func": "P"}
+y = {"func": "1010000000"}
 
 while True:
     for i in range(1):
@@ -17,7 +17,7 @@ while True:
         client, addr = s[i].accept()
         # client.settimeout(200)
 
-        with open('data.json', 'r') as file:
+        with open('./data.json', 'r') as file:
             data = file.read()
             try:
                 y = json.loads(data)
