9d023baa32839c3344f1032b19150c8473fc4c83
Av-nish <soy.avnish@gmail.com>
2021-11-20 17:41:17 +0530

    Bot can now go to M D K

--- Round 2/main.py
+++ Round 2/main.py
@@ -5,33 +5,38 @@ import threading
 from utils import *
 from motion import *
 
+
+
+
 dictionary = {'bot1': '10100000000', 'bot2': '10100000000'}
 
 def cvFunc():
     global dictionary
     induct = read_data()[0]
     destNo = 0
-    laut_jao = 0
+
+
 
     location = {i: [[0, 0] for j in range(5)] for i in range(0, 50)}
-    destination = [{'M': [1196, 201], 'D':[1204, 412], 'K':[1209, 629]}]
+    destination = [{'M': [1196, 201], 'D':[1204, 340], 'K':[1209, 500]}]
 
-    vid = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+    vid = cv2.VideoCapture(2)
     vid.set(3, 1420) 
-    vid.set(4, 1420)
+    vid.set(4, 800)
 
     while True:
         _, frame = vid.read()
 
         location = detectMarker(frame, location, markerSize=4,totalMarker=50, draw=True)
+        
         corners = [location[i][4] for i in range(46, 50)]
         frame = warp(frame, corners)
-
-        dictionary = move_bot(location, destination[0]['M'],destNo)#[induct[destNo][1]])
-        print(location[1][4])
+        dictionary, destNo = move_bot(location, destination[0][induct[destNo][1]],destNo)
+        print(location[7][4])
 
         cv2.imshow('frame', frame)
         cv2.waitKey(1)
+        
 
 def socketFunc():
     global dictionary
--- Round 2/motion.py
+++ Round 2/motion.py
@@ -1,15 +1,17 @@
 from utils import getAngle
 import time
 
-s=0
-then=0
-stop=0
-laut_jao=0
+s = 0
+then = 0
+stop = 0
+laut_jao = 0
 
-def move_bot(location, destination,destNo):
-    global stop,then,s,laut_jao
+
+def move_bot(location, destination, destNo):
+    global stop, then, s, laut_jao
     cx, cy = location[1][4]
-    shortestAngle, intHeadingDeg = getAngle(location[1])
+    shortestAngle, intHeadingDeg = getAngle(location[1], destination, laut_jao)
+    print(shortestAngle)
 
     if stop == 1:
         now = time.time()
@@ -21,10 +23,11 @@ def move_bot(location, destination,destNo):
             stop = 0
 
     if destination[0] > 1167:
+        # print(laut_jao, "\n")
         if(laut_jao == 0):
             if(cy < destination[1]):
-                h1 = str(max(0, min(255, 100 - int(intHeadingDeg * 4))))
-                h2 = str(max(0, min(255, 100 + int(intHeadingDeg * 4))))
+                h1 = str(max(0, min(255, 100 + int((intHeadingDeg * 4)))))
+                h2 = str(max(0, min(255, 100 - int((intHeadingDeg * 4)))))
                 h1 = '0'*(3-len(h1)) + h1
                 h2 = '0'*(3-len(h2)) + h2
                 dictionary = {'bot1': f'1010{h2}{h1}0'}
@@ -35,24 +38,33 @@ def move_bot(location, destination,destNo):
                 print("rotate")
 
             else:
-                dictionary = {'bot1': f'10011301301'}
+                # original  dictionary = {'bot1': f'10011301301'}
+                dictionary = {'bot1': f'10010000001'}
+
                 laut_jao = 1
 
         else:
-            if(intHeadingDeg<-10):
+            if(intHeadingDeg < -50):
+                print("anti- rotate")
+
                 dictionary = {'bot1': f'10011301301'}
-            
-            elif(cy>73):
-                h1 = str(max(0, min(255, 100 - int(shortestAngle * 4))))
-                h2 = str(max(0, min(255, 100 + int(shortestAngle * 4))))
+
+            elif(cy > 73):
+                h1 = str(max(0, min(255, 100 + int(shortestAngle * 4))))
+                h2 = str(max(0, min(255, 100 - int(shortestAngle * 4))))
                 h1 = '0'*(3-len(h1)) + h1
                 h2 = '0'*(3-len(h2)) + h2
-                dictionary = {'bot1': f'0101{h2}{h1}2'}
-            
+                dictionary = {'bot1': f'0101{h2}{h1}0'}
+                print("backward")
+
             else:
-                laut_jao=0
-                stop=1
-                dest_No=destNo+1
+                laut_jao = 0
+                stop = 1
+                destNo = destNo+1
                 dictionary = {'bot1': f'10010000000'}
 
-    return dictionary
+    # elif(destination[0]>650):
+
+
+
+    return dictionary, destNo
